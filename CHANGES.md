16e162bb9b815146f7e1e9070f1bc795c50079d0
Andy Fragen <andy@thefragens.com>
2022-08-26 17:01:04 -0700

    prevent possible race condition

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,5 @@
 [unreleased]
+* add `sleep(2)` to possibly prevent a race condition
 
 #### 0.8.0 / 2022-08-24
 * large refactor into smaller chunks
--- wp-admin/includes/class-rollback-auto-update.php
+++ wp-admin/includes/class-rollback-auto-update.php
@@ -90,6 +90,11 @@ class WP_Rollback_Auto_Update {
 			return $result;
 		}
 
+		// I believe this helps to avoid a potential race condition on servers that may start to
+		// process the next plugin for auto-updating before the handler can pick up an error from
+		// the previously processed plugin.
+		sleep(2);
+
 		error_log( $hook_extra['plugin'] . ' processing...' );
 		$this->no_error     = false;
 		static::$current    = get_site_transient( 'update_plugins' );
