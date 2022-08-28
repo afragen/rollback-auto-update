79296ff1be66e1cf0174d54058c0b748521763a9
Andy Fragen <andy@thefragens.com>
2022-08-28 15:15:06 -0700

    hit snooze one more time ⏰

--- wp-admin/includes/class-rollback-auto-update.php
+++ wp-admin/includes/class-rollback-auto-update.php
@@ -93,7 +93,7 @@ class WP_Rollback_Auto_Update {
 		// I believe this helps to avoid a potential race condition on servers that may start to
 		// process the next plugin for auto-updating before the handler can pick up an error from
 		// the previously processed plugin.
-		sleep(2);
+		sleep(3);
 
 		error_log( $hook_extra['plugin'] . ' processing...' );
 		$this->no_error     = false;
